33b42347da1d4286face2ff326e92c570a72fc29
Pratik Singh <singhpratik0001@gmail.com>
2023-08-06 12:20:44 +0530

    episode1 completed

---
+++ App.js
@@ -0,0 +1,11 @@
+const heading = React.createElement("h1",
+    {
+        // this is the place where you give attributes to your tags
+        id: 'heading',
+        xyz:'abc'
+    }
+    , "Hello from React")
+        
+const root = ReactDOM.createRoot(document.getElementById("root"))
+
+root.render(heading)
