a03130d1ba39c1a96a6b228c16557eea12cac7d0
Pratik Singh <singhpratik0001@gmail.com>
2023-09-01 22:08:26 +0530

    testing

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ const heading = React.createElement("h1",
     {
         // this is the place where you give attributes to your tags
         id: 'heading',
+        id: 'heading',
         xyz:'abc'
     }
     , "Hello from React")
